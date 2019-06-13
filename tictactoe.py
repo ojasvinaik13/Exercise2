54a0d28b99268c94813d618e0de9698c7d3a3c07
Ojasvi Naik <51634900+ojasvinaik13@users.noreply.github.com>
2019-06-13 18:16:41 +0530

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,65 @@
+mat=[]
+for i in range(3):
+    mat.append([])
+for i in range(3):
+    for j in range(3):
+        mat[i].append(j)
+        mat[i][j]="_"
+
+for i in range(9):
+    if i%2==0:
+        print("Player 1:")
+        print("Enter the row and column where you want to insert X")
+        r=int(input())
+        c=int(input())
+        mat[r-1][c-1] = "X"
+
+        for p in range(3):
+            for q in range(3):
+                print(mat[p][q], end=" ")
+            print()
+    else:
+        print("Player 2:")
+        print("Enter the row and column where you want to insert 0")
+        r = int(input())
+        c = int(input())
+        mat[r-1][c-1] = "0"
+
+        for p in range(3):
+            for q in range(3):
+                print(mat[p][q], end=" ")
+            print()
+
+    for a in range(3):
+        b=0
+        if mat[a][b]=="X" and mat[a][b+1]=="X" and mat[a][b+2]=="X":
+            print("Player 1 wins!")
+            exit()
+        elif mat[a][b]=="0" and mat[a][b+1]=="0" and mat[a][b+2]=="0":
+            print("Player 2 wins!")
+            exit()
+
+    for a in range(3):
+        b=0
+        if mat[b][a]=="X" and mat[b+1][a]=="X" and mat[b+2][a]=="X":
+            print("Player 1 wins!")
+            exit()
+        elif mat[b][a]=="0" and mat[b+1][a]=="0" and mat[b+2][a]=="0":
+            print("Player 2 wins!")
+            exit()
+
+    if mat[0][0]=="X" and mat[1][1]=="X" and mat[2][2]=="X":
+        print("Player 1 wins!")
+        exit()
+    elif mat[0][2]=="X" and mat[1][1]=="X" and mat[2][0]=="X":
+        print("Player 1 wins!")
+        exit()
+    elif mat[0][0]=="0" and mat[1][1]=="0" and mat[2][2]=="0":
+        print("Player 2 wins!")
+        exit()
+    elif mat[0][2]=="0" and mat[1][1]=="0" and mat[2][0]=="0":
+        print("Player 2 wins!")
+        exit()
+
+print("Its a draw!")
+
