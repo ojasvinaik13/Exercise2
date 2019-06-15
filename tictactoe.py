b21f2122111d7422826d712a820d8ed7db396eeb
Ojasvi Naik <51634900+ojasvinaik13@users.noreply.github.com>
2019-06-15 11:10:12 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -4,6 +4,7 @@ for i in range(3):
 for i in range(3):
     for j in range(3):
         mat[i].append(j)
+        # Initialising all positions of matrix to underscore
         mat[i][j]="_"
 
 for i in range(9):
@@ -12,8 +13,9 @@ for i in range(9):
         print("Enter the row and column where you want to insert X")
         r=int(input())
         c=int(input())
-        mat[r-1][c-1] = "X"
-
+        mat[r-1][c-1] = "X" 
+    
+# Printing the matrix after every turn of each player
         for p in range(3):
             for q in range(3):
                 print(mat[p][q], end=" ")
@@ -25,11 +27,13 @@ for i in range(9):
         c = int(input())
         mat[r-1][c-1] = "0"
 
+# Printing the matrix after every turn of each player        
         for p in range(3):
             for q in range(3):
                 print(mat[p][q], end=" ")
             print()
-
+            
+# Checking row wise elements
     for a in range(3):
         b=0
         if mat[a][b]=="X" and mat[a][b+1]=="X" and mat[a][b+2]=="X":
@@ -39,6 +43,7 @@ for i in range(9):
             print("Player 2 wins!")
             exit()
 
+# Checking column wise elements            
     for a in range(3):
         b=0
         if mat[b][a]=="X" and mat[b+1][a]=="X" and mat[b+2][a]=="X":
@@ -47,7 +52,8 @@ for i in range(9):
         elif mat[b][a]=="0" and mat[b+1][a]=="0" and mat[b+2][a]=="0":
             print("Player 2 wins!")
             exit()
-
+            
+# Checking the diagonal elements
     if mat[0][0]=="X" and mat[1][1]=="X" and mat[2][2]=="X":
         print("Player 1 wins!")
         exit()
